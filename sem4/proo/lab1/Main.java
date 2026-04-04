320e8e65d2b7986cda9b24a171164e20bf697880
3Baku <kubahalaczkiewicz@gmail.com>
2026-04-04 19:53:26 +0200

    Rozwiazany lab1, wyslane

--- sem4/proo/lab1/Main.java
+++ sem4/proo/lab1/Main.java
@@ -3,7 +3,13 @@ public class Main {
     {
         //tworzenie obiektu klasy
         Pixel firstPixel = new Pixel(1,2,3);
-        //wyswietlenie
-        System.out.println(firstPixel.luminance);
+        //System.out.println(firstPixel.luminance);
+        //Point myPoint = new Point(5,6);
+        //myPixel.location = myPoint;
+        Pixel secondPixel = new Pixel(firstPixel);
+        secondPixel.setLuminance(50);
+        secondPixel.location.x = 2137;
+        firstPixel.location.y = 67;
+        System.out.println("Pierwszy pixel:\n" + firstPixel + "\n\n Drugi pixel:\n" +secondPixel);
     }
 }
--- sem4/proo/lab1/Pixel.java
+++ sem4/proo/lab1/Pixel.java
@@ -6,8 +6,10 @@ public class Pixel {
     private int luminance;
     public final Point location;
 
+    //konstruktor glowny
     public Pixel(int luminance, int coordinateX, int coordinateY)
     {
+        // zmienna obiektu jest rowna argumentowi
         this.luminance = luminance;
         this.location = new Point(coordinateX, coordinateY);
     }
@@ -18,9 +20,21 @@ public class Pixel {
         luminance = myPixel.luminance;
         location = new Point(myPixel.location);
     }
+    public int getLuminance()
+    {
+        return this.luminance;
+    }
+    public void setLuminance(int newLuminance)
+    {
+        if(newLuminance > 0 && newLuminance <255)
+            this.luminance = newLuminance;
+        else
+            System.out.println("proba nadpisania luminance wartoscia spoza zakresu.");
+    }
+    @Override
     public String toString()
     {
-        return ("Wspolrzedna x: " + location.x + "\nWspolrzednia y: "+ location.y + "\nJasnosc: "+ luminance);
+        return ("Wspolrzedna x: " + this.location.x + "\nWspolrzednia y: "+ this.location.y + "\nJasnosc: "+ this.luminance);
     }
     
 }
